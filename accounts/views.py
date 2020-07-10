a54592f67c1885b2571e9e94f50322a60750b983
viikkkas <61241966+viikkkas@users.noreply.github.com>
2020-07-10 16:26:15 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
-# Create your views here.
 def register(request):
     if request.method == 'POST':
         firstname = request.POST['firstname']
